d721f0e37687a6186a6f0e13173605fe10caea5b
dlflq11@gmail.com <dlflq11@gmail.com>
2025-09-15 15:28:44 +0900

    restcontroller

---
+++ demo/src/main/java/com/example/demo/response/Star.java
@@ -0,0 +1,15 @@
+package com.example.demo.response;
+import lombok.Getter;
+
+@Getter
+public class Star {
+    String name;
+    int age;
+
+    public Star(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Star() {}
+}
