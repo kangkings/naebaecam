17a250401906f3a1a632bf49b3677431c990f42a
dlflq11@gmail.com <dlflq11@gmail.com>
2025-09-19 12:19:05 +0900

    passwordEncoder 수동 등록 및 테스트

---
+++ spring-auth/src/test/java/com/sparta/spring_auth/PasswordEncoderTest.java
@@ -0,0 +1,30 @@
+package com.sparta.spring_auth;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootTest
+public class PasswordEncoderTest {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @DisplayName("수동 등록한 passwordEncoder 테스트")
+    @Test
+    void test(){
+        // given
+        String password = "password";
+        // when
+
+        String encodedPassword = passwordEncoder.encode(password);
+        System.out.println("encodedPassword = " + encodedPassword);
+
+        String input = "kang";
+        // then
+        boolean matches = passwordEncoder.matches(input,encodedPassword);
+        System.out.println("matches = " + matches);
+    }
+}
