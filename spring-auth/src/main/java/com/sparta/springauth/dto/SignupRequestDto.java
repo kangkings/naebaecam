cd7a0409f5a02aec1c998c100ca4c8c0626a60ff
kangkigs <dlflq11@gmail.com>
2025-09-21 22:24:03 +0900

    정규식 validation 적용

--- spring-auth/src/main/java/com/sparta/springauth/dto/SignupRequestDto.java
+++ spring-auth/src/main/java/com/sparta/springauth/dto/SignupRequestDto.java
@@ -2,6 +2,7 @@ package com.sparta.springauth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,8 +13,8 @@ public class SignupRequestDto {
     private String username;
     @NotBlank
     private String password;
-    @Email
-    @NotBlank
+    @Pattern(regexp = "^(?!.*\\s)[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
+    ,message = "유효한 이메일을 입력하세요 (공백 불가).")
     private String email;
     private boolean admin = false;
     private String adminToken = "";
