5876523238a20c52d2ce590ee35969ddcc131c95
dlflq11@gmail.com <dlflq11@gmail.com>
2025-09-23 11:24:14 +0900

    스케줄 기능 구현

--- myselectshop/src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ myselectshop/src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -4,6 +4,7 @@ import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.naver.dto.ItemDto;
 import com.sparta.myselectshop.repository.ProductRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -43,5 +44,12 @@ public class ProductService {
 //        }
         List<ProductResponseDto> responseList = productRepository.findAll().stream().map(ProductResponseDto::new).toList();
         return responseList;
+    }
+
+    @Transactional
+    public void updateBySearch(Long id, ItemDto itemDto) {
+        Product product = productRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 상품은 존재하지 않습니다."));
+        product.update(itemDto);
     }
 }
