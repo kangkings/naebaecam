39f8b05f4fa51ce08df7ed9be4ed3dcc9198eab7
dlflq11@gmail.com <dlflq11@gmail.com>
2025-09-23 11:06:24 +0900

    관심상품 희망 최저가 업데이트 API

---
+++ myselectshop/src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.sparta.myselectshop.controller;
+
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping("/products")
+    public ProductResponseDto createProduct(@RequestBody ProductRequestDto productRequestDto){
+        return productService.createProduct(productRequestDto);
+    }
+
+    @PutMapping("/products/{id}")
+    public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto){
+        return productService.updateProduct(id,requestDto);
+    }
+}
